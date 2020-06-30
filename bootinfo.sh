454b3487a097ea0bfcffa94fa71f3c1204fe6a1b
Patrolin <patrik@balas.cz>
2020-06-30 23:41:24 +0200

    Update bootinfo.sh

--- bootinfo.sh
+++ bootinfo.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 [ -e /sys/firmware/efi ] && echo "Firmware interface:  UEFI" || echo "Firmware interface:  BIOS/CSM/LEGACY";
-lscpu | grep Architecture:
 lsblk -o PTTYPE,PATH,SIZE,PARTTYPENAME,FSTYPE,FSVER,MOUNTPOINT,PARTFLAGS; # does not work if your lsblk is garbage
+lscpu | grep -e Architecture -e "Model name";
+lspci | grep -e 3D -e VGA;
