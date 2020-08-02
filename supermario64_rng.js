1919e733a3468aace6ec89ab3bbc5a0076d44934
Patrolin <patrik@balas.cz>
2020-08-02 19:05:43 +0200

    Update supermario64_rng.js

--- supermario64_rng.js
+++ supermario64_rng.js
@@ -5,10 +5,13 @@ function right(n){
 	return n & 0x00FF;
 }
 function rng(a=0){
+    "This results in a single mathemagical cycle";
 	b = a ^ (right(a) << 8);
 	a = (right(b) << 8) | (left(b) >> 8);
 	b = a ^ (right(b) << 1);
 	return ((b >> 1) ^ 0xFF80) ^ (b & 0x1 ? 0x8180 : 0x1FF4);
 }
 
-rng(0);
+console.log(seed = rng());
+console.log(seed = rng(seed));
+console.log(seed = rng(seed));
