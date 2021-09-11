d654a31875218cd4bb4a8711abe560f746f56b7a
Patrolin <patrik@balas.cz>
2021-09-11 18:29:06 +0200

    added concurrency

--- ytdownload.py
+++ ytdownload.py
@@ -5,9 +5,12 @@ import pytube # python -m pip install git+https://github.com/pytube/pytube
 import os
 import re
 import time
+import random
+import asyncio
 import subprocess # pip install youtube-dl
+from pprint import pprint
 
-def parseBool(s: str, default=True) -> bool:
+def parseBool(s: str, default: bool) -> bool:
   s = s.strip()
   if s in {'Y', 'y'}:
     return True
@@ -24,35 +27,82 @@ def fs_escape(s: str) -> str:
   #return re.sub(NTFS_ESCAPE, '_', s) if os.name == 'nt' else re.sub(EXT_POSIX_ESCAPE, '_', s)
   s = re.sub(FOO_ESCAPE, '_', s)
   s = re.sub(r'"', '\'', s)
-  s = re.sub(r'$', 'S', s)
+  s = re.sub(r'\$', 'S', s)
   s = re.sub(r'\|', '-', s)
   return s
+def youtubedl_escape(s: str) -> str:
+  return re.sub(r"%", r"%%", s)
 
-def download_url(url: str, path: str, i: int, N: int) -> None:
-  global do_while
+async def sh(cmd: str):
+  P = asyncio.subprocess.PIPE
+  process = await asyncio.create_subprocess_shell(cmd, stdout=P, stderr=P)
+  stdout, stderr = await process.communicate()
+  if (process.returncode != 0):
+    raise Exception(stdout.decode('utf-8') + '\n' + stderr.decode('utf-8'))
+
+video_count = 0
+
+archive = {}
+video_info_count = 0
+video_info_count_lock = asyncio.Lock()
+async def get_video_info_and_download(url: str, audio_only: bool, playlist_path: str, storage: dict):
+  global connections, video_info_count, download_count
+  if url in archive:
+    file_name = archive[url]
+  else:
+    for j in range(8):
+      await connections.acquire()
+      try:
+        youtube = pytube.YouTube(url)
+        file_name = fs_escape(f'{youtube.title} [{youtube.author}]')
+        archive[url] = file_name
+        connections.release()
+        break
+      except:
+        connections.release()
+        if j == 7:
+          raise
+        await asyncio.sleep(2**j)
+  await video_info_count_lock.acquire()
+  video_info_count += 1
+  video_info_count_lock.release()
+  print(f'({video_info_count}/{video_count}) +{url} {file_name}', flush=True)
+
+  if (file_name not in storage) or (not (storage[file_name] &
+                                          (AUDIO_EXTENSIONS_WHITELIST if audio_only else VIDEO_EXTENSIONS_WHITELIST))):
+    file_path = f'{playlist_path}{youtubedl_escape(file_name)}'
+    c = download_media(url, audio_only, file_path)
+    await c
+  else:
+    await download_count_lock.acquire()
+    download_count += 1
+    download_count_lock.release()
+
+download_count = 0
+download_count_lock = asyncio.Lock()
+async def download_media(url: str, audio_only: bool, file_path: str):
+  global connections, download_count
   cmd = 'youtube-dl'
   cmd += f' "{url}"'
   if audio_only:
-    cmd += f' -f "best[height<={max_video_height}]/best"'
-    cmd += f' -x --audio-format "mp3"' # youtube-dl is dumb and only supports exporting to mp3 # TODO: download only audio when possible
+    cmd += f' -f "bestaudio"'
+    cmd += f' -x --audio-format "mp3"'
     cmd += f' --add-metadata --metadata-from-title "%(title)s"'
   else:
     cmd += f' -f "bestvideo[height<={max_video_height}]+bestaudio/best[height<={max_video_height}]/best"'
     cmd += f' --merge-output-format "mkv" --recode-video "mkv"' # debian is stupid and has decades old ffmpeg and doesn't let you recode into mp4
     cmd += f' --write-sub --write-auto-sub --sub-lang "en,cs,rechat" --embed-subs'
-    #cmd += f' --write-thumbnail --embed-thumbnail' # debian's youtube-dl is terrible and has broken dependencies for mp4 and doesn't support mkv
-  #cmd += f' -o "{path}%(title)s [%(channel)s].%(ext)s"'
-  cmd += f' -o "{path}.%(ext)s"'
-  #cmd += f' --download-archive ~/storage/downloads/video/{playlist_path}archive.txt' # termux doesn't implement asyncio, so this doesn't work either
-  print(f'\n({i+1}/{N}) {cmd}', flush=True)
-  do_while |= subprocess.run(cmd, shell=True).returncode
+  cmd += f' -o "{file_path}.%(ext)s"'
+  await download_count_lock.acquire()
+  download_count += 1
+  download_count_lock.release()
+  print(f'\n({download_count}/{video_count}) {cmd}', flush=True)
+  await connections.acquire()
+  await sh(cmd)
+  connections.release()
 
-FFMPEG_AUDIO_EXTENSIONS = {'mp3'}
-FFMPEG_VIDEO_EXTENSIONS = {
-    "3g2", "3gp", "3gp2", "asf", "avi", "cavs", "dvr-ms", "exr", "ffindex", "ffpreset", "flv", "gxf", "h261", "h263",
-    "h264", "ifv", "ivf", "kux", "m2t", "m2ts", "m4v", "mkv", "mod", "mov", "mp4", "mpg", "mxf", "nut", "png", "tod",
-    "vob", "webm", "wmv", "xmv", "y4m"
-}
+AUDIO_EXTENSIONS_WHITELIST = {'mp3'}
+VIDEO_EXTENSIONS_WHITELIST = {'mkv'}
 
 if len(sys.argv) == 1:
   exit('Not enough arguments!')
@@ -62,82 +112,76 @@ else:
   exit('Too many arguments!')
 
 try:
+  is_playlist = True
   playlist = pytube.Playlist(input_url)
   playlist_title = playlist.title
 except Exception:
-  playlist = None
+  is_playlist = False
+  playlist = {input_url}
   playlist_title = ''
 
-do_while = True
-while do_while:
-  playlist_path = ''
-  if playlist:
-    while playlist_path == '':
-      playlist_path = input(f'\nPlaylist name? [{playlist_title}]: ') or playlist_title
-    playlist_path += '/'
+user_path = os.path.expanduser('~')
+is_termux = user_path.find('com.termux') > -1
+music_path = f'{user_path}/storage/music/' if is_termux else f'{user_path}/Music/'
+downloads_path = f'{user_path}/storage/downloads/' if is_termux else f'{user_path}/Downloads/'
+
+async def main():
+  global connections, video_count, download_count
+  playlist_name = ''
+  if is_playlist:
+    while playlist_name == '':
+      playlist_name = input(f'Playlist name? [{playlist_title}]: ') or playlist_title
+
   audio_only = None
   while audio_only == None:
     audio_only = parseBool(input(f'Audio only? [Yn]: '), True)
+
   max_video_height = None
   while max_video_height == None:
     try:
       max_video_height = round(float(input(f'Max video height? [480]: ') or '480'))
     except ValueError:
       pass
-  
-  if audio_only:
-    path = f'~/storage/downloads/audio/{playlist_path}'
-  else:
-    path = f'~/storage/downloads/video/{playlist_path}'
-  path = os.path.expanduser(path)
-  playlist2 = playlist or {input_url}
-  
-  media = {}
+
+  root_path = f'{music_path if audio_only else downloads_path}'
+  playlist_path = f'{root_path}{playlist_name}/'
   try:
-    with os.scandir(path) as d:
-      for direntry in d:
-        if direntry.is_file():
-          name, ext = direntry.name.rsplit('.', 1)
-          if name not in media:
-            media[name] = {ext}
-          else:
-            media[name].add(ext)
-  except FileNotFoundError:
-    pass
-  try:
-    os.mkdir(path)
+    os.mkdir(playlist_path)
   except FileExistsError:
     pass
-  try:
-    with open(f'{path}qArchive.txt', 'r', encoding='utf8') as f:
-      archive = {p[0]: (p[1][:-1] if p[1][-1] == '\n' else p[1]) for p in (x.split(' ', 1) for x in f.readlines())}
-  except FileNotFoundError:
-    archive = {}
-  goto = True
-  for i, url in enumerate(playlist2):
-    if (url not in archive) or (archive[url] not in media) or (
-        not (media[archive[url]] & (FFMPEG_AUDIO_EXTENSIONS if audio_only else FFMPEG_VIDEO_EXTENSIONS))):
-      goto = False
-      for j in range(8):
-        try:
-          youtube = pytube.YouTube(url)
-          archive[url] = fs_escape(f'{youtube.title} [{youtube.author}]')
-          goto = True
-        except:
-          print(f'({j+1}/8) Failed')
-          time.sleep(2**j)
-      if not goto:
-        break
-      print(f'({i+1}/{len(playlist2)}) +{url}')
-  if not goto:
-    continue
-  with open(f'{path}qArchive.txt', 'w+', encoding='utf8') as f:
-    f.writelines([f'{url} {name}\n' for (url, name) in archive.items()])
-  media_delta = {}
-  for i, url in enumerate(playlist2):
-    if (archive[url] not in media) or (not (media[archive[url]] &
-                                            (FFMPEG_AUDIO_EXTENSIONS if audio_only else FFMPEG_VIDEO_EXTENSIONS))):
-      media_delta[url] = archive[url]
-  do_while = False
-  for i, url in enumerate(media_delta):
-    download_url(url, f'{path}{media_delta[url]}', i, len(media_delta))
+
+  connections = asyncio.BoundedSemaphore(3)
+
+  do_while = True
+  while do_while:
+    storage = {}
+    for (dirpath, dirnames, filenames) in os.walk(root_path):
+      for file_name in filenames:
+        name, ext = file_name.rsplit('.', 1)
+        if name not in storage:
+          storage[name] = {ext}
+        else:
+          storage[name].add(ext)
+
+    download_count = 0
+    tasks = []
+    shuffled_playlist = list(playlist)
+    random.shuffle(shuffled_playlist)
+    for url in shuffled_playlist:
+      video_count += 1
+      task = asyncio.ensure_future(get_video_info_and_download(url, audio_only, playlist_path, storage))
+      tasks.append(task)
+
+    await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
+    errors = [t for t in tasks if t.exception() != None]
+    error_count = len(errors)
+    if error_count:
+      pprint(errors)
+      print(f'{download_count} downloads {error_count} errors', flush=True)
+      do_while = parseBool(input(f'Continue? [Yn]: '), True)
+    else:
+      print(f'{download_count} downloads {error_count} errors', flush=True)
+      input(f'Exit? [Y]: ')
+      do_while = False
+
+asyncio.run(main())
