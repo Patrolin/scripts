cbf9d36c539e4cd0ff16476ba4d0c94c49ed9c7a
Patrolin <patrik@balas.cz>
2022-08-26 17:27:47 +0200

    fast ytdownload.py

--- ytdownload.py
+++ ytdownload.py
@@ -1,189 +1,176 @@
-#!/usr/bin/python
-
-import sys
-import pytube # python -m pip install git+https://github.com/pytube/pytube
-import os
+from yt_dlp import YoutubeDL
 import re
-import time
-import random
+from typing import NamedTuple, cast
 import asyncio
-import subprocess # pip install youtube-dl
-from pprint import pprint
+from os.path import expanduser, exists as fileexists
+from sys import argv
 
-def parseBool(s: str, default: bool) -> bool:
-  s = s.strip()
-  if s in {'Y', 'y'}:
-    return True
-  elif s in {'N', 'n'}:
-    return False
-  else:
-    return None if s else default
+# playlists
+class VideoInfo(NamedTuple):
+    title: str
+    url: str
 
-NTFS_ESCAPE = r'[\/:*?"<>|]'
-EXT_POSIX_ESCAPE = r'[/<>|*$]'
+def url_param(key: str, url: str) -> str:
+    match = re.search(f"[?&]{key}=([^&]+)", url)
+    return match[1] if match != None else ""
+
+def video_url(video_id: str) -> str:
+    return f"https://www.youtube.com/watch?v={video_id}"
+
+def playlist_url(playlist_id: str) -> str:
+    return f"https://www.youtube.com/playlist?list={playlist_id}"
+
+def exit_with_prompt(error: str, error_code: int = 1):
+    if error: print(error)
+    input("Press enter to exit...")
+    exit(error_code)
+
+# prompts
+def promptBool(prefix: str, default: bool) -> bool:
+    b: bool | None = None
+    while b == None:
+        b = parseBool(input(prefix), default)
+    return b
+
+def parseBool(s: str, default: bool) -> bool | None:
+    s = s.strip()
+    if s in {"Y", "y"}:
+        return True
+    elif s in {"N", "n"}:
+        return False
+    else:
+        return None if s else default
+
+def promptInt(prefix: str, default: int):
+    v: int | None = None
+    while v == None:
+        v = parseInt(input(prefix), default)
+    return v
+
+def parseInt(s: str, default: int) -> int | None:
+    s = s.strip()
+    if s == "":
+        return default
+    try:
+        return int(s)
+    except ValueError:
+        return None
+
+# download
+async def sh(cmd: str):
+    P = asyncio.subprocess.PIPE
+    process = await asyncio.create_subprocess_shell(cmd, stdout=P, stderr=P)
+    stdout, stderr = await process.communicate()
+    if (process.returncode != 0):
+        raise Exception(stdout.decode('utf-8') + '\n' + stderr.decode('utf-8'))
+
+#NTFS_ESCAPE = r'[\/:*?"<>|]'
 FOO_ESCAPE = r'[\/:*?<>*]'
 
 def fs_escape(s: str) -> str:
-  #return re.sub(NTFS_ESCAPE, '_', s) if os.name == 'nt' else re.sub(EXT_POSIX_ESCAPE, '_', s)
-  s = re.sub(FOO_ESCAPE, '_', s)
-  s = re.sub(r'"', '\'', s)
-  s = re.sub(r'\$', 'S', s)
-  s = re.sub(r'\|', '-', s)
-  return s
-def youtubedl_escape(s: str) -> str:
-  return re.sub(r"%", r"%%", s)
+    #return re.sub(NTFS_ESCAPE, '_', s) if os.name == 'nt' else re.sub(EXT_POSIX_ESCAPE, '_', s)
+    s = re.sub(FOO_ESCAPE, '_', s)
+    s = re.sub(r'"', '\'', s)
+    s = re.sub(r'\$', 'S', s)
+    s = re.sub(r'\|', '-', s)
+    return s
 
-async def sh(cmd: str):
-  P = asyncio.subprocess.PIPE
-  process = await asyncio.create_subprocess_shell(cmd, stdout=P, stderr=P)
-  stdout, stderr = await process.communicate()
-  if (process.returncode != 0):
-    raise Exception(stdout.decode('utf-8') + '\n' + stderr.decode('utf-8'))
+AUDIO_EXTENSION = "mp3"
 
-video_count = 0
+def AUDIO_CMD(video_info: VideoInfo, file_path: str):
+    acc = "yt-dlp"
+    acc += f" {video_info.url}"
+    acc += f" -f bestaudio"
+    acc += f" -x --audio-format {AUDIO_EXTENSION}"
+    acc += f" --add-metadata --metadata-from-title '%(title)s'"
+    acc += f" -o \"{file_path}\""
+    print(acc)
+    return acc
 
-archive = {}
-video_info_count = 0
-video_info_count_lock = asyncio.Lock()
-async def get_video_info_and_download(url: str, audio_only: bool, playlist_path: str, storage: dict):
-  global connections, video_info_count, download_count
-  if url in archive:
-    file_name = archive[url]
-  else:
-    for j in range(8):
-      await connections.acquire()
-      # todo: get english title # https://stackoverflow.com/questions/1216029/get-title-from-youtube-videos
-      try:
-        youtube = pytube.YouTube(url)
-        file_name = fs_escape(f'{youtube.title} [{youtube.author}]')
-        archive[url] = file_name
-        connections.release()
-        break
-      except:
-        connections.release()
-        if j == 7:
-          raise
-        await asyncio.sleep(2**j)
-  await video_info_count_lock.acquire()
-  video_info_count += 1
-  video_info_count_lock.release()
-  print(f'\n({video_info_count}/{video_count}) +{url} {file_name}', flush=True)
+VIDEO_EXTENSION = "mkv"
 
-  if (file_name not in storage) or (not (storage[file_name] &
-                                          (AUDIO_EXTENSIONS_WHITELIST if audio_only else VIDEO_EXTENSIONS_WHITELIST))):
-    file_path = f'{playlist_path}{youtubedl_escape(file_name)}'
-    c = download_media(url, audio_only, file_path)
-    await c
-  else:
-    await download_count_lock.acquire()
-    download_count += 1
-    download_count_lock.release()
+def VIDEO_CMD(video_info: VideoInfo, file_path: str, max_video_height: int):
+    acc = "yt-dlp"
+    acc += f" {video_info.url}"
+    acc += f" -f \"bestvideo[height<={max_video_height}]+bestaudio/best[height<={max_video_height}]/best\""
+    acc += f" --merge-output-format \"{VIDEO_EXTENSION}\" --recode-video {VIDEO_EXTENSION}"
+    acc += f" --write-subs --write-auto-subs --embed-subs --compat-options no-keep-subs --sub-lang \"en,cs\""
+    acc += f" -o \"{file_path}\""
+    print(acc)
+    return acc
 
-download_count = 0
-download_count_lock = asyncio.Lock()
-async def download_media(url: str, audio_only: bool, file_path: str):
-  global connections, download_count
-  cmd = 'youtube-dl'
-  cmd += f' "{url}"'
-  if audio_only:
-    cmd += f' -f "bestaudio"'
-    cmd += f' -x --audio-format "mp3"'
-    cmd += f' --add-metadata --metadata-from-title "%(title)s"'
-  else:
-    cmd += f' -f "bestvideo[height<={max_video_height}]+bestaudio/best[height<={max_video_height}]/best"'
-    cmd += f' --merge-output-format "mkv" --recode-video "mkv"' # debian is stupid and has decades old ffmpeg and doesn't let you recode into mp4
-    cmd += f' --write-sub --write-auto-sub --sub-lang "en,cs,rechat" --embed-subs'
-  cmd += f' -o "{file_path}.%(ext)s"'
-  await download_count_lock.acquire()
-  download_count += 1
-  download_count_lock.release()
-  print(f'\n({download_count}/{video_count}) {cmd}', flush=True)
-  await connections.acquire()
-  await sh(cmd)
-  connections.release()
+user_path = expanduser("~")
+is_termux = user_path.find("com.termux") > -1
+music_path = f"{user_path}/storage/music" if is_termux else f"{user_path}/Music"
+downloads_path = f"{user_path}/storage/downloads" if is_termux else f"{user_path}/Downloads"
 
-AUDIO_EXTENSIONS_WHITELIST = {'mp3'}
-VIDEO_EXTENSIONS_WHITELIST = {'mkv'}
+async def download_video_if_not_exist(
+    video_info: VideoInfo, playlist_name: str | None, \
+    audio_only: bool, max_video_height: int, \
+    n: int, maxN: int
+):
+    dir_path = (music_path if audio_only else downloads_path) + (f"/{playlist_name}" if playlist_name != None else "")
 
-if len(sys.argv) == 1:
-  exit('Not enough arguments!')
-if len(sys.argv) == 2:
-  input_url = sys.argv[1]
-else:
-  exit('Too many arguments!')
+    def get_file_name(title: str) -> str:
+        return f"{title}." + (AUDIO_EXTENSION if audio_only else VIDEO_EXTENSION)
 
-try:
-  is_playlist = True
-  playlist = pytube.Playlist(input_url)
-  playlist_title = playlist.title
-except Exception:
-  is_playlist = False
-  playlist = {input_url}
-  playlist_title = ''
+    def get_file_path(file_name: str) -> str:
+        return f"{dir_path}/{fs_escape(file_name)}"
 
-user_path = os.path.expanduser('~')
-is_termux = user_path.find('com.termux') > -1
-music_path = f'{user_path}/storage/music/' if is_termux else f'{user_path}/Music/'
-downloads_path = f'{user_path}/storage/downloads/' if is_termux else f'{user_path}/Downloads/'
+    file_name = get_file_name(video_info.title)
+    file_path = get_file_path(file_name)
+    temp_file_path = get_file_path(get_file_name(f"{video_info.title}.temp"))
+
+    def print_diff(new: bool):
+        print(f" {' +'[new]} {file_name}{f' ({n}/{maxN})' if playlist_name != None else ''}")
+
+    file_exists = fileexists(file_path) and not fileexists(temp_file_path)
+    print_diff(not file_exists)
+    if file_exists:
+        return
+    if audio_only:
+        await sh(AUDIO_CMD(video_info, file_path))
+    else:
+        await sh(VIDEO_CMD(video_info, file_path, max_video_height))
+
+TREAT_VIDEO_WITH_PLAYLIST_AS_PLAYLIST = False
 
 async def main():
-  global connections, video_count, video_info_count, download_count
-  playlist_name = ''
-  if is_playlist:
-    while playlist_name == '':
-      playlist_name = input(f'Playlist name? [{playlist_title}]: ') or playlist_title
-
-  audio_only = None
-  while audio_only == None:
-    audio_only = parseBool(input(f'Audio only? [Yn]: '), True)
-
-  max_video_height = None
-  while max_video_height == None:
-    try:
-      max_video_height = round(float(input(f'Max video height? [480]: ') or '480'))
-    except ValueError:
-      pass
-
-  root_path = f'{music_path if audio_only else downloads_path}'
-  playlist_path = f'{root_path}{playlist_name}/'
-  try:
-    os.mkdir(playlist_path)
-  except FileExistsError:
-    pass
-
-  connections = asyncio.BoundedSemaphore(3)
-
-  do_while = True
-  while do_while:
-    storage = {}
-    for (dirpath, dirnames, filenames) in os.walk(root_path):
-      for file_name in filenames:
-        name, ext = file_name.rsplit('.', 1)
-        if name not in storage:
-          storage[name] = {ext}
-        else:
-          storage[name].add(ext)
-
-    video_count = len(playlist)
-    video_info_count = 0
-    download_count = 0
-    tasks = []
-    shuffled_playlist = list(playlist)
-    random.shuffle(shuffled_playlist)
-    for url in shuffled_playlist:
-      task = asyncio.ensure_future(get_video_info_and_download(url, audio_only, playlist_path, storage))
-      tasks.append(task)
-
-    await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
-    errors = [t for t in tasks if t.exception() != None]
-    error_count = len(errors)
-    if error_count:
-      pprint(errors)
-      print(f'\n{download_count} downloads {error_count} errors', flush=True)
-      do_while = parseBool(input(f'Continue? [Yn]: '), True)
+    if len(argv) == 1:
+        exit_with_prompt('Not enough arguments!')
+    if len(argv) == 2:
+        url = argv[1]
     else:
-      print(f'\n{download_count} downloads {error_count} errors', flush=True)
-      input(f'Exit? [Y]: ')
-      do_while = False
+        exit_with_prompt('Too many arguments!')
 
-asyncio.run(main())
+    playlist_name: str | None = None
+    videos: list[VideoInfo] = []
+    video_id = url_param("v", url)
+    playlist_id = url_param("list", url)
+    is_playlist = (playlist_id != "") if TREAT_VIDEO_WITH_PLAYLIST_AS_PLAYLIST else (video_id == "")
+    if (is_playlist and playlist_id == "") or (not is_playlist and video_id == ""):
+        exit_with_prompt(f"Error: invalid url; {url}")
+    standardized_url: str = playlist_url(cast(str, playlist_id)) if is_playlist else video_url(cast(str, video_id))
+    with YoutubeDL({"extract_flat": True, "quiet": True}) as yt:
+        playlist_or_video = yt.extract_info(standardized_url, download=False)
+        if is_playlist:
+            playlist_name = playlist_or_video["title"]
+            for video in playlist_or_video["entries"]:
+                videos.append(VideoInfo(video["title"], video["url"]))
+        else:
+            videos.append(VideoInfo(playlist_or_video["title"], standardized_url))
+    print(f" Playlist: {playlist_name}" if is_playlist else f" Video: {videos[0].title}")
+
+    audio_only = promptBool(" Audio only? [Yn]: ", True)
+    max_video_height = promptInt(" Max video height? [480]: ", 480) if not audio_only else 480
+
+    #work_index_lock = asyncio.Lock()
+    for i, v in enumerate(videos):
+        try:
+            await download_video_if_not_exist(v, playlist_name, audio_only, max_video_height, i + 1, len(videos))
+        except Exception as err:
+            print(err)
+    exit_with_prompt("", 0)
+
+if __name__ == "__main__":
+    asyncio.run(main())
